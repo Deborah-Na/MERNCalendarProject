162dfd9c8ce877c6d4ed509500e04631f6316fbe
Seann Avery <SeannAvery@gmail.com>
2022-03-31 16:25:46 -0700

    created front end

--- server/routes/calendar.routes.js
+++
@@ -1,9 +0,0 @@
-const Calendar = require ("../controllers/calendar.controllers")
-
-module.exports = function(app) {
-    app.get("/api/calendar", Calendar.allCalendarContent)
-    app.post("/api/calendar/create", Calendar.createCalendarContent)
-    app.get("/api/calendar/entry/:id", Calendar.getOneEntry)
-    app.put("/api/calendar/entry/:id", Calendar.updateEntry)
-    app.delete('/api/calendar/:id/', Calendar.deleteEntry);
-}
